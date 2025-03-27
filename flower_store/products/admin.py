44bed3b1dfa5191199400139d3a01331a0439480
ali <ali@email.com>
2025-03-27 13:46:30 +0330

    add a filter for the comment model up to comments based on their confirmation status in products/admin.py

--- flower_store/products/admin.py
+++ flower_store/products/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
 from .models import Product, Category, Comment, DiscountCode
+from django.utils.html import format_html
 
-
-new_models = [Product, Category, Comment, DiscountCode]
+new_models = [Product, Category, DiscountCode]
 
 for model in new_models:
     admin.site.register(model)
+
+
+class ConfirmationStatusFilter(admin.SimpleListFilter):
+    title = 'comment situation'  # عنوانی که در پنل ادمین نمایش داده می‌شود
+    parameter_name = 'confirmation_status'  # پارامتر مورد استفاده در URL
+
+    def lookups(self, request, model_admin):
+        # تعریف گزینه‌های فیلتر
+        return (
+            ('accepted', 'کامنت‌های تاییدشده ✅'),
+            ('not_reviewed', 'کامنت‌های بررسی‌نشده ⚠️'),
+        )
+
+    def queryset(self, request, queryset):
+        # فیلتر کردن بر اساس انتخاب کاربر
+        if self.value() == 'accepted':
+            return queryset.filter(admin_confirmation=True)
+        if self.value() == 'not_reviewed':
+            return queryset.filter(admin_confirmation=False)
+        return queryset  # اگر هیچ فیلتری انتخاب نشده، همه کامنت‌ها نمایش داده می‌شوند
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = (ConfirmationStatusFilter,)
